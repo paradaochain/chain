3931be0cd57d5bc9c9e47f67fcd93964102809eb
bwty <whalelephant@users.noreply.github.com>
2022-07-02 23:21:32 +0700

    add contracts wip

--- scripts/register-paradao.sh
+++ scripts/register-paradao.sh
@@ -1,31 +1,31 @@
-#!/usr/bin/env bash
-
-set -e
-
-temp="./.tmp"
-
-if [ ! -x "$temp" ]; then
-    mkdir ./.tmp
-fi
-
-
-# Generate wasm
-echo -e "\n\nGenerating Parachain Runtime WASM"
-./target/release/parachain-collator export-genesis-wasm \
-    --chain ./chainspecs/rococo-local-parachain-2000-raw.json > ./.tmp/para-2000-wasm
-
-# Generate genesis state
-echo -e "\n\nGenerating Parachain Genesis state"
-./target/release/parachain-collator export-genesis-state \
-    --chain ./chainspecs/rococo-local-parachain-2000-raw.json > ./.tmp/para-2000-genesis
-
-echo -e "\n\nRunning NodeJS app to reserve and register parachain slot"
-cd ./register
-yarn install
-cd ..
-node register 127.0.0.1 6644 \
-       ../.tmp/para-2000-wasm \
-       ../.tmp/para-2000-genesis
+# #!/usr/bin/env bash
+#
+# set -e
+#
+# temp="./.tmp"
+#
+# if [ ! -x "$temp" ]; then
+#     mkdir ./.tmp
+# fi
+#
+#
+# # Generate wasm
+# echo -e "\n\nGenerating Parachain Runtime WASM"
+# ./target/release/parachain-collator export-genesis-wasm \
+#     --chain ./chainspecs/rococo-local-parachain-2000-raw.json > ./.tmp/para-2000-wasm
+#
+# # Generate genesis state
+# echo -e "\n\nGenerating Parachain Genesis state"
+# ./target/release/parachain-collator export-genesis-state \
+#     --chain ./chainspecs/rococo-local-parachain-2000-raw.json > ./.tmp/para-2000-genesis
+#
+# echo -e "\n\nRunning NodeJS app to reserve and register parachain slot"
+# cd ./register
+# yarn install
+# cd ..
+# node register 127.0.0.1 6644 \
+#        ../.tmp/para-2000-wasm \
+#        ../.tmp/para-2000-genesis
 
 
 get_id () {
@@ -45,6 +45,9 @@ echo -e "\n\nStarting collator..."
 --port 20333 \
 --ws-port 8844 \
 --rpc-port 8833 \
+--rpc-cors=all \
+--unsafe-rpc-external \
+--unsafe-ws-external \
 -- \
 --execution wasm \
 --chain ./chainspecs/rococo-local-raw.json \
